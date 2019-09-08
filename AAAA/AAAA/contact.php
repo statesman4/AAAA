c611afe36035855d32d840f32dde23ebccf65556
Nadeem Fatmi <nafatmi@live.com>
2019-09-07 20:35:29 -0400

    email engine

---
+++ AAAA/AAAA/contact.php
@@ -0,0 +1,12 @@
+﻿<?php
+
+// Edit these 2 lines
+$email  = 'test@yahoo.com';
+$return = 'http://www.google.com';
+
+// Do not edit anything below this line
+if(!$_POST) header("Location: /");
+mail($email, "Contact form filled in", "Name: " . $_POST['name'] . "\nEmail: " . $_POST['email'] . "\n\n" . $_POST['comments']);
+header("Location: $return");
+
+?>
