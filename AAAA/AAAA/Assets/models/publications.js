a22d04b38ea9b4202a682f8fdbfce2a1c629ac99
Nadeem <nafatmi@gmail.com>
2019-12-07 16:30:33 -0500

    commit

---
+++ AAAA/AAAA/Assets/models/publications.js
@@ -0,0 +1,76 @@
+{
+  "count":10,
+  "publications": [
+        {
+          "Id":1,
+          "Name":"2001 Mashal",
+          "Date":"2001",
+          "Description":"",
+          "File":"https://drive.google.com/open?id=1Ubzw6olHN0I9AmvQaG4c4bzw7Jq65Y2L"
+        },
+        {
+          "Id":2,
+          "Name":"2004 Mashal",
+          "Date":"2004",
+          "Description":"",
+          "File":"https://drive.google.com/open?id=1BsdvPmbeWEMyBjz6sWi2moNZT3izvJnV"
+        },
+        {
+          "Id":3,
+          "Name":"2008 Alig Atlanta",
+          "Date":"2008",
+          "Description":"",
+          "File":"https://drive.google.com/open?id=1jQHlIsPWYzgDWvb1i9Pc1MMmV-08redr"
+        },
+        {
+          "Id":4,
+          "Name":"2009 Alig Atlanta",
+          "Date":"2009",
+          "Description":"",
+          "File":"https://drive.google.com/open?id=1WdbZTGsZGJILVtn32hYFatWhSW7AzNCw"
+        },
+        {
+          "Id":5,
+          "Name":"2010 Qandeel",
+          "Date":"2010",
+          "Description":"",
+          "File":"https://drive.google.com/open?id=1lHB2YL_Q_VBbqz4vrYN6DTa8lbwhReiD"
+        },
+        {
+          "Id":6,
+          "Name":"2015 Alig Atlanta",
+          "Date":"2015",
+          "Description":"",
+          "File":"https://drive.google.com/open?id=1bOGEfbLJKApjHf-KV3dPaBX_dO59vJRG"
+        },
+        {
+          "Id":7,
+          "Name":"2016 Alig Atlanta",
+          "Date":"2016",
+          "Description":"",
+          "File":"https://drive.google.com/open?id=171V9r1zzC8bbR4B7kjKGr4Wo-Fhuez00"
+        },
+        {
+          "Id":8,
+          "Name":"2017 Alig Atlanta",
+          "Date":"2017",
+          "Description":"",
+          "File":"https://drive.google.com/open?id=1OyeB9S2NiU-D0Zuz7KblrGjuC8vUduc5"
+        },
+        {
+          "Id":9,
+          "Name":"2018 Alig Atlanta",
+          "Date":"2018",
+          "Description":"",
+          "File":"https://drive.google.com/open?id=1MFWT-PtyCW_B7mW_7SULm7mjw4kI5mqI"
+        },
+        {
+          "Id":10,
+          "Name":"2019 Alig Atlanta",
+          "Date":"2019",
+          "Description":"",
+          "File":"https://drive.google.com/open?id=1jRq6Kw4kvRAvhVexor1FITBuh_HP9gk7"
+        }
+      ]
+    }
+        
